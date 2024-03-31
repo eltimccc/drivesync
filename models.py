576ee171f9bbc9cc6e6d93967127d773248f4581
eltimccc <eltimccc@yandex.ru>
2024-03-31 22:57:06 +0300

    50%

--- models.py
+++ models.py
@@ -8,14 +8,16 @@ class Car(db.Model):
     def __repr__(self):
         return f"Car('{self.brand}', '{self.car_number}')"
 
+
 class Booking(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     car_id = db.Column(db.Integer, db.ForeignKey('car.id'), nullable=False)
     car = db.relationship('Car', backref=db.backref('bookings', lazy=True))
-    start_date = db.Column(db.Date, nullable=False)
-    end_date = db.Column(db.Date, nullable=False)
     description = db.Column(db.Text, nullable=True)
-    extendable = db.Column(db.Boolean, default=False, nullable=False)
+    start_date = db.Column(db.DateTime, nullable=False)
+    end_date = db.Column(db.DateTime, nullable=False)
 
     def __repr__(self):
-        return f"Booking('{self.car.car_number}', '{self.start_date}', '{self.end_date}')"
+        return f"'{self.start_date}', '{self.end_date}'"
+
+
