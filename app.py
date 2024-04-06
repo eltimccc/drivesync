54036f57f688d2da8ddfc7f3892e81dfeeefb5c5
eltimccc <eltimccc@yandex.ru>
2024-04-07 00:00:47 +0300

    сделал роутеры и html

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 load_dotenv()
 
-# Установка секретного ключа из переменных среды
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 db = SQLAlchemy(app)
 
 
@@ -17,6 +16,5 @@ from routes import *
 with app.app_context():
     db.create_all()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
-
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from app import db
 
+
 class Car(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     brand = db.Column(db.String(100), nullable=False)
@@ -11,12 +12,11 @@ class Car(db.Model):
 
 class Booking(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    car_id = db.Column(db.Integer, db.ForeignKey('car.id'), nullable=False)
-    car = db.relationship('Car', backref=db.backref('bookings', lazy=True))
+    car_id = db.Column(db.Integer, db.ForeignKey("car.id"), nullable=False)
+    car = db.relationship("Car", backref=db.backref("bookings", lazy=True))
     description = db.Column(db.Text, nullable=True)
     start_date = db.Column(db.DateTime, nullable=False)
     end_date = db.Column(db.DateTime, nullable=False)
 
     def __repr__(self):
         return f"'{self.start_date}', '{self.end_date}'"
-
