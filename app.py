ef51b24cc3c8d750f16330bd726452d7220c94c9
eltimccc <eltimccc@yandex.ru>
2024-04-07 09:55:04 +0300

    edit templ

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 load_dotenv()
 
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
@@ -17,4 +18,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    extra_files = ['./templates/index.html']  # Указываем путь к папке с шаблонами
+    app.run(debug=True, extra_files=extra_files)
