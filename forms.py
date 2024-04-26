2993fa2c69293a653317a5ea8ce9d8b86c5d6fbc
eltimccc <eltimccc@yandex.ru>
2024-04-27 00:34:25 +0300

    add car from valid

--- forms.py
+++ forms.py
@@ -1,42 +1,38 @@
 from datetime import datetime
+import re
 from flask_wtf import FlaskForm
 from wtforms import (
     DateTimeLocalField,
     SelectField,
     StringField,
     DateTimeField,
+    SubmitField,
     TextAreaField,
+    ValidationError,
 )
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, Regexp
+
+from models import Car
 
 
-class EditBookingForm(FlaskForm):
-    description = TextAreaField("Description", validators=[DataRequired()])
-    start_date = DateTimeField(
-        "Start Date and Time",
-        format="%Y-%m-%dT%H:%M",
-        validators=[DataRequired()],
-        default=datetime.now,
-    )
-    end_date = DateTimeField(
-        "End Date and Time",
-        format="%Y-%m-%dT%H:%M",
-        validators=[DataRequired()],
-        default=datetime.now,
+class CarForm(FlaskForm):
+    brand = StringField('Пример Hyundai Solaris', validators=[
+                        DataRequired(message='Обязательное поле')])
+    car_number = StringField(
+        'Например А123МР',
+        validators=[
+            DataRequired(message='Обязательное поле'),
+            Length(min=6, max=6),
+            Regexp('^([А-ЯЁ]{1}|[A-Z]{1})\d{3}([А-ЯЁ]{2}|[A-Z]{2})$',
+                   re.IGNORECASE,
+                   message='Номер должен состоять букв и цифр без пробелов')
+        ]
     )
 
+    def validate_car_number(self, car_number):
+        car_number.data = car_number.data.upper()
+        car = Car.query.filter_by(car_number=car_number.data).first()
+        if car:
+            raise ValidationError('Машина с таким номером уже существует.')
 
-class BookingForm(FlaskForm):
-    start_datetime = DateTimeLocalField(
-        "Дата и время начала", format="%Y-%m-%dT%H:%M", validators=[DataRequired()]
-    )
-    end_datetime = DateTimeLocalField(
-        "Дата и время окончания", format="%Y-%m-%dT%H:%M", validators=[DataRequired()]
-    )
-    car = SelectField(
-        "Автомобиль",
-        coerce=int,
-        validators=[DataRequired()],
-        render_kw={"class": "form-control"},
-    )
-    description = TextAreaField("Заметка")
+    submit = SubmitField('Добавить')
