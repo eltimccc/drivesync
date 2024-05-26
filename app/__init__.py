cf8fb53826591a8628e00a93e8613f482e1b5db8
eltimccc <eltimccc@yandex.ru>
2024-05-26 09:45:25 +0300

    auth

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.login_message_category = 'info'
 
 
 def create_app():
@@ -10,6 +16,8 @@ def create_app():
     app.config.from_object('config.Config')
 
     db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
 
     from .views.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
@@ -22,6 +30,9 @@ def create_app():
 
     from app.views.report import report_blueprint
     app.register_blueprint(report_blueprint)
+
+    from app.views.auth import auth_blueprint
+    app.register_blueprint(auth_blueprint)
 
     with app.app_context():
         db.create_all()
---
+++ app/views/auth.py
@@ -0,0 +1,46 @@
+from flask import Blueprint, render_template, redirect, url_for, flash
+from flask_login import login_user, current_user, logout_user, login_required
+from app import db, bcrypt, login_manager
+from app.models import User
+from app.forms import RegistrationForm, LoginForm
+
+auth_blueprint = Blueprint("auth", __name__, url_prefix="/auth")
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+@auth_blueprint.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('auth.login'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Регистрация завершена, теперь вы можете войти', 'success')
+        return redirect(url_for('auth.login'))
+    return render_template('register.html', title='Register', form=form)
+
+@auth_blueprint.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.get_bookings'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('main.get_bookings'))
+        else:
+            flash('Неполучилось, проверьте ввденные данные', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@auth_blueprint.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('main.get_bookings'))
