7380c3e37fa2a02254195f0f47fdcf3d05424fbc
eltimccc <eltimccc@yandex.ru>
2024-06-16 00:21:26 +0300

    uri db dotenv

--- config.py
+++ config.py
@@ -1,18 +1,3 @@
-# import os
-# from dotenv import load_dotenv
-
-# basedir = os.path.abspath(os.path.dirname(__file__))
-# load_dotenv(os.path.join(basedir, ".env"))
-
-
-# class Config:
-#     SECRET_KEY = os.getenv("SECRET_KEY")
-#     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
-#         basedir, "instance", "db.sqlite3"
-#     )
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-#     TEMPLATES_AUTO_RELOAD = True
-
 import os
 from dotenv import load_dotenv
 
@@ -23,7 +8,7 @@ load_dotenv(os.path.join(basedir, ".env"))
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/drivesync_db"
+        "DATABASE_URL",
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASK_ENV = os.getenv("FLASK_ENV")
