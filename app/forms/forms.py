1c3328e3eacf95b23a89729315442318c572810a
eltimccc <eltimccc@yandex.ru>
2024-06-22 09:38:21 +0300

    теперь в логине имя пользователя

--- app/forms/forms.py
+++ app/forms/forms.py
@@ -186,7 +186,7 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     remember = BooleanField("Запомнить меня")
     submit = SubmitField("Login")
