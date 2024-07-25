5d8cebece1e3b3b706bb7e5937b8e3ec92b85418
eltimccc <eltimccc@yandex.ru>
2024-07-25 23:29:09 +0300

    сделал flask-talisman

---
+++ app/utils/security/csp_config.py
@@ -0,0 +1,39 @@
+csp = {
+    'default-src': [
+        "'self'",
+        'fonts.googleapis.com',
+        'fonts.gstatic.com',
+        'cdn.jsdelivr.net',
+        'cdnjs.cloudflare.com',
+        'stackpath.bootstrapcdn.com',
+        'code.jquery.com'
+    ],
+    'style-src': [
+        "'self'",
+        "'unsafe-inline'",
+        'fonts.googleapis.com',
+        'cdn.jsdelivr.net',
+        'cdnjs.cloudflare.com',
+        'stackpath.bootstrapcdn.com'
+    ],
+    'font-src': [
+        "'self'",
+        'fonts.gstatic.com',
+        'cdn.jsdelivr.net',
+        'cdnjs.cloudflare.com'
+    ],
+    'img-src': [
+        "'self'",
+        'data:',
+        'cdn.jsdelivr.net',
+        'cdnjs.cloudflare.com'
+    ],
+    'script-src': [
+        "'self'",
+        "'unsafe-inline'",
+        'cdn.jsdelivr.net',
+        'cdnjs.cloudflare.com',
+        'code.jquery.com',
+        'stackpath.bootstrapcdn.com'
+    ]
+}
