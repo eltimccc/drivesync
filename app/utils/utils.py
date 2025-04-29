e80886421186ff0870a15c00f23298dea8fb956f
eltimccc <eltimccc@yandex.ru>
2025-04-29 22:12:34 +0300

    edit utils

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -2,36 +2,27 @@ from app.models import Booking
 from app.models import Car
 
 BOOKING_STATUSES = {
-    "Бронь": "#99959e",  # Светло-серый цвет для забронированных автомобилей
-    "Аренда": "#007bff",  # Синий цвет для текущих арендуемых автомобилей
-    "Ожидание": "#ffc107",  # Желтый цвет для ожидающих подтверждения
-    "Завершено": "#28a745",  # Зеленый цвет для завершенных бронирований
-    "Отказ": "#dc3545",  # Красный цвет для отмененных бронирований
+    "Бронь": "#99959e",
+    "Аренда": "#007bff",
+    "Ожидание": "#ffc107",
+    "Завершено": "#28a745",
+    "Отказ": "#dc3545",
 }
 
-# Словарь для русских названий дней недели
-RU_WEEKDAYS = {
-    0: 'Пн',
-    1: 'Вт',
-    2: 'Ср',
-    3: 'Чт',
-    4: 'Пт',
-    5: 'Сб',
-    6: 'Вс'
-}
+RU_WEEKDAYS = {0: "Пн", 1: "Вт", 2: "Ср", 3: "Чт", 4: "Пт", 5: "Сб", 6: "Вс"}
 RU_MONTHS = {
-    1: 'Январь',
-    2: 'Февраль',
-    3: 'Марть',
-    4: 'Апрель',
-    5: 'Май',
-    6: 'Июнь',
-    7: 'Июль',
-    8: 'Август',
-    9: 'Сентябрь',
-    10: 'Октябрь',
-    11: 'Ноябрь',
-    12: 'Декабрь'
+    1: "Январь",
+    2: "Февраль",
+    3: "Март",
+    4: "Апрель",
+    5: "Май",
+    6: "Июнь",
+    7: "Июль",
+    8: "Август",
+    9: "Сентябрь",
+    10: "Октябрь",
+    11: "Ноябрь",
+    12: "Декабрь",
 }
 
 
@@ -75,27 +66,23 @@ def enrich_car_with_bookings(car, start_date, end_date):
     """
 
     last_booking = (
-        Booking.query.filter(Booking.car_id == car.id,
-                             Booking.end_date <= start_date)
+        Booking.query.filter(Booking.car_id == car.id, Booking.end_date <= start_date)
         .order_by(Booking.end_date.desc())
         .first()
     )
 
     next_booking = (
-        Booking.query.filter(Booking.car_id == car.id,
-                             Booking.start_date >= end_date)
+        Booking.query.filter(Booking.car_id == car.id, Booking.start_date >= end_date)
         .order_by(Booking.start_date)
         .first()
     )
 
     car.last_booking_end = (
-        last_booking.end_date.strftime(
-            "%d.%m.%Y %H:%M") if last_booking else None
+        last_booking.end_date.strftime("%d.%m.%Y %H:%M") if last_booking else None
     )
 
     car.next_booking_start = (
-        next_booking.start_date.strftime(
-            "%d.%m.%Y %H:%M") if next_booking else None
+        next_booking.start_date.strftime("%d.%m.%Y %H:%M") if next_booking else None
     )
 
 
